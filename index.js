09cbcf5d4e5424e028e2b137ae3f379348897f83
Diego Maroto <diego.diemg@gmail.com>
2018-05-21 19:23:12 +0200

    Añadido stock local, base de datos local y conexión remota

--- index.js
+++ index.js
@@ -5,58 +5,68 @@ var cors = require('cors')
 app = express()
 app.use(bp.json())
 app.use(cors())
-var mysql = require('mysql')
-var mysqlObj = {host:'localhost',user:'mtis',password:'mtis_password',database:'mtis_grupo'}
-//var conn = mysql.createConnection(mysqlObj)
+var basededatos = {
+    productos: [{
+        name: '',
+        unidades: '',
+        referencia: ''
+    }]
+}
 
-var productos = [{name:'sony experia',unidades:1},
-    {name:'samsung noseque',unidades:5},
-    {name:'xiaomi',unidades:-1},
-    {name:'bq badalum',unidades:4}]
-
-app.get('/',function(req,resp){
-    resp.sendFile('views/index.html', {root: __dirname })
+app.get('/', function (req, resp) {
+    resp.sendFile('views/index.html', {
+        root: __dirname
+    })
 })
-app.get('/productos/stock',function(req,resp){
-    var constock = productos.filter(p => p.unidades > 0)
+app.get('/productos/stock', function (req, resp) {
+    var constock = basededatos.productos.filter(p => p.unidades > 0)
     resp.send(constock)
 })
-app.get('/productos/almacen',function(req,resp){
+app.get('/productos/almacen', function (req, resp) {
     var endpoint = 'http://localhost:9090/productos'
     var tienda = req.body.tiendaId
     var referenciaProducto = req.body.referenciaProducto
     var unidades = req.body.unidades
-    request.get(endpoint, {tienda,referenciaProducto,unidades}, function(err,httpResponse,body){
-        if(err){
+    request.get(endpoint, {
+        tienda,
+        referenciaProducto,
+        unidades
+    }, function (err, httpResponse, body) {
+        if (err) {
             resp.status(500)
             resp.send(err)
-        }else{
+        } else {
             resp.send(body)
         }
     })
 })
-app.get('/productos/',function(req,resp){
-    resp.send(productos)
+app.get('/productos/', function (req, resp) {
+    resp.send(basededatos.productos)
 })
-app.post('/productos/pedir',function(req,resp){
+app.post('/productos/pedir', function (req, resp) {
     var endpoint = 'http://localhost:9090/pedido'
     var tienda = req.body.tiendaId
     var referenciaProducto = req.body.referenciaProducto
     var unidades = req.body.unidades
-    request.post(endpoint, {tienda,referenciaProducto,unidades}, function(err,httpResponse,body){
-        if(err){
+    request.post(endpoint, {
+        tienda,
+        referenciaProducto,
+        unidades
+    }, function (err, httpResponse, body) {
+        if (err) {
             resp.status(500)
             resp.send(err)
-        }else{
+        } else {
             resp.send(body)
         }
     })
 })
-app.put('/productos',function(req,resp){
+app.put('/productos', function (req, resp) {
     resp.status(501)
     resp.end()
 })
+
 // RUNING SERVER
 app.listen(3000, function () {
-    console.log('MTIS PR2, por Diego Maroto, sirviendo por el puerto 3000')
+    console.log('MTIS GRUPAL, por Diego Maroto, sirviendo por el puerto 3000')
 })
