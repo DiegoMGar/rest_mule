9a081760de8ffde5e54b067a7dbc3cf822624cd1
DIEGO MAROTO GARCIA <dmg65@clL04D-13>
2018-05-22 17:29:05 +0200

    Almacen con listado de pedidos

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ var basededatos = {
         unidades: 0,
         referencia: 'sgs2',
         imgUrl: 'img/samsungs2.jpg'
-    }]
+    }],
+    pedidos: [{ mensaje: 'Texto de prueba', fecha: '2018-12-01', referencia: 'sgs3' },
+    { mensaje: 'Segundo mensaje', fecha: '2018-01-25', referencia:'sxz1' }]
 }
 app.get('/', function (req, resp) {
     resp.sendFile('views/index.html', {
@@ -43,21 +45,30 @@ app.get('/productos/almacen', function (req, resp) {
         }
     })
 })
-app.get('/productos/', function (req, resp) {
+app.get('/productos', function (req, resp) {
     resp.send(basededatos.productos)
 })
-app.get('/productos/:ref',function(req,resp){
+app.get('/pedidos', function (req, resp) {
+    var pedidos = []
+    for (var pedido of basededatos.pedidos) {
+        var arrayProducto = basededatos.productos.filter(p => p.referencia == pedido.referencia)
+        Object.assign(pedido, arrayProducto[0])
+        pedidos.push(pedido)
+    }
+    resp.send(pedidos)
+})
+app.get('/productos/:ref', function (req, resp) {
     var result = []
-    for(var producto of basededatos.productos){
-        if(producto.referencia == req.params.ref){
+    for (var producto of basededatos.productos) {
+        if (producto.referencia == req.params.ref) {
             result.push(producto)
             break
         }
     }
-    if(result.length<1){
+    if (result.length < 1) {
         resp.status(404)
         resp.end()
-    }else{
+    } else {
         resp.send(result)
     }
 })
@@ -66,6 +77,7 @@ app.post('/productos/pedir', function (req, resp) {
     var referenciaProducto = req.body.referenciaProducto
     var unidades = req.body.unidades
     request.post(endpoint, {
+        tienda: '1',
         referenciaProducto,
         unidades
     }, function (err, httpResponse, body) {
@@ -73,6 +85,14 @@ app.post('/productos/pedir', function (req, resp) {
             resp.status(500)
             resp.send(err)
         } else {
+            console.log(JSON.stringify(body))
+            var fecha = ''
+            if (body.respuesta.fecha)
+                fecha = body.respuesta.fecha
+            var mensaje = body.respuesta.mensaje
+            var salida = body.respuesta.salida
+            var obj = { mensaje: mensaje, fecha: fecha, referencia: referenciaProducto }
+            basededatos.pedidos.push(obj)
             resp.send(body)
         }
     })
@@ -84,5 +104,5 @@ app.put('/productos', function (req, resp) {
 
 // RUNING SERVER
 app.listen(3000, function () {
-    console.log('MTIS GRUPAL, por Diego Maroto, sirviendo por el puerto 3000')
+    console.log('MTIS GRUPAL almacén rest, por Diego Maroto, sirviendo por el puerto 3000')
 })
